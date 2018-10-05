acac0aa108383897786b24b34d109de5725b71ac
Brad Nissen <bradanissen@gmail.com>
2018-10-05 14:41:08 -0700

    running first tests with updated dependencies

---
+++ app/src/androidTest/java/com/ad340/micromomentums/MainActivityTest.java
@@ -0,0 +1,33 @@
+package com.ad340.micromomentums;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.filters.LargeTest;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest
+{
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+
+
+    @Test
+    public void homeActivityTest()
+    {
+        Espresso.onView(withId(R.id.hello_world)).check(matches(withText("Hello World!")));
+
+    }
+}
