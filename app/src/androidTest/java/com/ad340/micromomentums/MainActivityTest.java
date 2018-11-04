11a68b27e4d1c594245be666cc02755066aadb33
Brad Nissen <bradanissen@gmail.com>
2018-11-04 13:42:32 -0800

    added tests for the new UI with tabs

--- app/src/androidTest/java/com/ad340/micromomentums/MainActivityTest.java
+++ app/src/androidTest/java/com/ad340/micromomentums/MainActivityTest.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -28,8 +30,7 @@ public class MainActivityTest
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
     @Test
-    public void homeActivityTest()
-    {
+    public void homeActivityTest() {
         //Espresso.onView(withId(R.id.hello_world)).check(matches(withText("Hello World!")));
 
 
@@ -47,4 +48,27 @@ public class MainActivityTest
 
         //onData(withId(R.id.listView)).atPosition(0).perform(click());
     }
+        @Test
+        public void onCreateClickTabs(){
+
+        onView(withText("How It Works")).check(matches(withText("How It Works")));
+
+        onView(withId(R.id.container)).perform(swipeLeft());
+        onView(withText("Stocks")).check(matches(withText("Stocks")));
+
+        SystemClock.sleep(1800);
+        Stock stock1 = new Stock("xx", "34", "23", "13", "4");
+        Stock stock2 = new Stock("xx", "34", "25", "23", "3");
+        stock1.calcualteRising();
+        stock2.calcualteRising();
+        stock1.compareTo(stock2);
+        stock2.compareTo(stock1);
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(1).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(2).perform(click());
+
+        onView(withId(R.id.container)).perform(swipeLeft());
+        onView(withText("About")).check(matches(withText("About")));
+    }
+
 }
