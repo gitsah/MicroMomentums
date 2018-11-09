f1c36ccc6060d17e030d4d9ae5727f581bd3f225
Brad Nissen <bradanissen@gmail.com>
2018-11-08 18:50:34 -0800

    Tabs (#52)
    * listView showing in tabs fragment
    
    * percent of intervals displaying, but bug found when app calls API from Firebase. It seems to crash the app.
    
    * Header bar fixed and colors added
    
    * banner and colors added and temp logo on unfinished pages
    
    * added tests for the new UI with tabs
    
    * percent column colors are matching logos and added text shadowing
    
    * fixed gradle for build, Added Tabs, changed colors for percentages

--- app/src/main/java/com/ad340/micromomentums/MainActivity.java
+++ app/src/main/java/com/ad340/micromomentums/MainActivity.java
@@ -1,14 +1,28 @@
 package com.ad340.micromomentums;
 
+import android.app.FragmentManager;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
+    private static final String TAG = "MainActivity";
+    private SectionsPageAdapter mSectionsPageAdapter;
+    private ViewPager mViewPager;
     ListView lst;
 
 
@@ -17,26 +31,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lst= (ListView) findViewById(R.id.listView);
+        //Tab Layout
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
-        FirebaseViewModel viewModel = new FirebaseViewModel();
-        viewModel.getStocks(
-                (ArrayList<Stock> stocks) -> {
-                    //Find isRising for each stock
-                    for(Stock stock : stocks) stock.calcualteRising();
+        // Set up  the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
 
-                    //Sort stocks by amount of change descending  //<-- TEMP SHUT OFF SHORT, BRING BACK TO TEST!
-                    stocks.sort(Collections.reverseOrder());
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
 
-                    //Create arrays from List of Stocks to pass to ListView
-                    String stockSymbols[] = stocks.stream().map(Stock::getSymbol).toArray(String[]::new);
-                    String stockValues[] = stocks.stream().map(Stock::getValue).toArray(String[]::new);
-                    String stockLast5s[] = stocks.stream().map(Stock::getLast5).toArray(String[]::new);
-                    String stockLast10s[] = stocks.stream().map(Stock::getLast10).toArray(String[]::new);
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "How It Works");
+        adapter.addFragment(new Tab2Fragment(), "Stocks");
+        adapter.addFragment(new Tab3Fragment(), "About");
 
-                    StockListView slv = new StockListView(this, stockSymbols, stockValues, stockLast5s, stockLast10s);
-                    lst.setAdapter(slv);
-                }
-        );
+        viewPager.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/ad340/micromomentums/StockListView.java
+++ app/src/main/java/com/ad340/micromomentums/StockListView.java
@@ -1,6 +1,7 @@
 package com.ad340.micromomentums;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +13,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
 
 public class StockListView extends ArrayAdapter<String> {
 
@@ -54,8 +57,20 @@ public class StockListView extends ArrayAdapter<String> {
         viewHolder.tvw3.setText(last5[position]);
         viewHolder.tvw4.setText(last10[position]);
 
+
         boolean isRising = doubleCheck(value[position],last5[position],last10[position]);
 
+        double percentChange = percentChange(value[position],last5[position],last10[position]);
+        viewHolder.tvw5.setText(String.valueOf(percentChange));
+
+        if (percentChange < 0) {
+            viewHolder.tvw5.setBackgroundColor(Color.parseColor("#CC0000"));
+            viewHolder.tvw5.setShadowLayer(1,1,1, Color.parseColor("#000000"));
+
+        } else if (percentChange > 0) {
+            viewHolder.tvw5.setBackgroundColor(Color.parseColor("#5FD25F"));
+            viewHolder.tvw5.setShadowLayer(1,1,1, Color.parseColor("#000000"));
+        }
 
         if (isRising) {
             viewHolder.iv2.setVisibility(convertView.GONE);
@@ -77,6 +92,7 @@ public class StockListView extends ArrayAdapter<String> {
         TextView tvw2;
         TextView tvw3;
         TextView tvw4;
+        TextView tvw5;
         ImageView iv1;
         ImageView iv2;
         TextView tvX;
@@ -86,9 +102,11 @@ public class StockListView extends ArrayAdapter<String> {
             tvw2 = (TextView) v.findViewById(R.id.value);
             tvw3 = (TextView) v.findViewById(R.id.last5);
             tvw4 = (TextView) v.findViewById(R.id.last10);
+            tvw5 = (TextView) v.findViewById(R.id.percentChange);
             iv1  = (ImageView)v.findViewById(R.id.momentum_true);
             iv2 = (ImageView) v.findViewById(R.id.momentum_false);
             tvX = (TextView) v.findViewById(R.id.momentum);
+
         }
     }
 
@@ -104,6 +122,34 @@ public class StockListView extends ArrayAdapter<String> {
         double currentVal = Double.valueOf(current);
         double last5Val = Double.valueOf(last5);
         double last10Val = Double.valueOf(last10);
+
         return (currentVal > last5Val && last5Val > last10Val);
+    }
+
+    /**
+     * Take a percent change between each interval of 5 minutes and average them.
+     * So the final percent displayed is the average change for each interval.
+     * @param current
+     * @param last5
+     * @param last10
+     * @return
+     */
+    private double percentChange(String current, String last5, String last10){
+        // convert the values from Strings to doubles
+        double currentVal = Double.valueOf(current);
+        double last5Val = Double.valueOf(last5);
+        double last10Val = Double.valueOf(last10);
+
+        // Difference from last 10 to last 5
+        double change5to10 = ((last5Val - last10Val) / last10Val) * 100;
+        double changeCurrentTo5 = ((currentVal - last5Val) / last5Val) * 100;
+        double avgPercentChange = (change5to10 + changeCurrentTo5) / 2;
+
+        // this returns a string
+        DecimalFormat df = new DecimalFormat("###.####");
+
+        return Double.valueOf(df.format(avgPercentChange));
+
+
     }
 }
